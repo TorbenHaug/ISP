06a9b675827edd2d8fb37631fc125fb00bc15d07
Isaisaisa <louisa@spahl-hamburg.de>
2015-04-15 16:56:44 +0200

    again

--- Aufgabe1/Tests_Verwandtschaftsbeziehung.pl
+++ Aufgabe1/Tests_Verwandtschaftsbeziehung.pl
@@ -5,9 +5,42 @@
   test(vater) :- vater(luca, emma).
   test(vater) :- vater(liam, julian).
   test(vater) :- not(vater(emma, luca)).
-  test(vater, [fail]) :- vater(alexander, david).
+  test(vater) :- not(vater(alexander, david)).
+
+:-end_tests(vater).
+:-begin_tests(mutter).
+  test(mutter) :- mutter(anna, emma).
+  test(mutter) :- mutter(vanessa, julian).
+  test(mutter) :- not(mutter(emma, luca)).
+  test(mutter) :- not(mutter(alexander, david)).
 
 :-end_tests(vater).
 
 %Tests verden mit run_tests(<testname>). ausgeführt
+
+
+
+
+
+% onkel(X,Y) -> X ist Onkel von Y
+
+% tante(X,Y) -> X ist Tante von Y
+
+
+
+% grossonkel(X,Y) -> X ist Großonkel von Y
+
+% grosstante(X,Y) -> X ist Großtante von Y
+
+
+
+% cousin(X,Y) -> X ist Cousin von Y.
+
+% cousine(X,Y) -> X ist Cousine von Y
+
+
+
+% schwager(X,Y) -> X ist Schwager von Y
+
+%schwaegerin(X,Y) -> X ist Schwägerin von Y
 
