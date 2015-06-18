59adaa8b2b76a3ce72381a4890a975418242deab
TorbenHaug <claustorbenhaug@hotmail.de>
2015-06-18 19:33:29 +0200

    Switches Player

--- Aufgabe4/Mill/src/de/mill/model/Player.java
+++ Aufgabe4/Mill/src/de/mill/model/Player.java
@@ -1,15 +1,40 @@
 package de.mill.model;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Created by abq329 on 18.06.2015.
  */
 public class Player {
+    private final MillColor color;
+    private final Stack<Stone> stock = new Stack<>();
+    private PlayerState state = PlayerState.Await;
 
+    public Player(MillColor color){
+        this.color = color;
+        for(int i=0; i < 9; i++){
+            addStoneToStock(new Stone(color));
+        }
+    }
     public boolean hasStonsInStock(){
-        return true;
+        return stock.size() > 0;
     }
 
-    public Stone getStoneFromStock(){
-        return new Stone(MillColor.Black);
+    Stone getStoneFromStock(){
+        return stock.pop();
+    }
+
+    void addStoneToStock(Stone stone){
+        stock.push(stone);
+    }
+
+    void setState(PlayerState state) {
+        this.state = state;
+    }
+    public PlayerState getState(){
+        return state;
     }
 }
---
+++ Aufgabe4/Mill/src/de/mill/model/PlayerState.java
@@ -0,0 +1,11 @@
+package de.mill.model;
+
+/**
+ * Created by abq329 on 18.06.2015.
+ */
+public enum PlayerState {
+    Await,
+    Set,
+    Move,
+    Remove;
+}
